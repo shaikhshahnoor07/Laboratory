761f8f96ef235632068cb6b9018e1ad59b3e5337
shaikhshahnoor07 <116806924+shaikhshahnoor07@users.noreply.github.com>
2022-10-28 14:07:12 +0530

    Create Print_Basic_Detail.java
    Q1)Write a program to accept your basic details (Name, No, Gmail, Address ,gender, percentage ) from user and display the output

---
+++ Print_Basic_Detail.java
@@ -0,0 +1,22 @@
+/*Q1)Write a program to accept your basic details (Name, No, Gmail,
+Address ,gender, percentage ) from user and display the output
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Following Detail.....");
+        System.out.println("Name : ");
+        String name = sc.next();
+        System.out.println("Contact No : ");
+        long no = sc.nextLong();
+        System.out.println("Gmail : ");
+        String mail = sc.next();
+        System.out.println("Address : ");
+        String Add = sc.next();
+        System.out.println("Enter Percentage : ");
+        double percent = sc.nextDouble();
+
+        System.out.println("Hello "+name+", Your percentage is : "+percent+"your number is "+no+" Your Gmail is : "+mail+"And you are from : "+Add);
+    }
+}
