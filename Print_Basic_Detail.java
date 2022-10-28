87ead0739784269aeaf9f0152766c39ddd15a20e
shaikhshahnoor07 <116806924+shaikhshahnoor07@users.noreply.github.com>
2022-10-28 15:32:07 +0530

    Update Print_Basic_Detail.java

--- Print_Basic_Detail.java
+++ Print_Basic_Detail.java
@@ -4,7 +4,7 @@ Address ,gender, percentage ) from user and display the output
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);                //Taking input for print detail
         System.out.println("Please Enter Following Detail.....");
         System.out.println("Name : ");
         String name = sc.next();
@@ -16,7 +16,7 @@ public class Main {
         String Add = sc.next();
         System.out.println("Enter Percentage : ");
         double percent = sc.nextDouble();
-
+                                                        //Here we are printing the given detail
         System.out.println("Hello "+name+", Your percentage is : "+percent+"your number is "+no+" Your Gmail is : "+mail+"And you are from : "+Add);
     }
 }
