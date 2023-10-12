2138c50f1ecc56f9d9039b02ca310ddb8c1bb97d
Shaikh Shahnoor Ahmed <116806924+shaikhshahnoor07@users.noreply.github.com>
2023-10-12 13:37:43 +0530

    Create Methods6.java

---
+++ Methods/Methods6.java
@@ -0,0 +1,19 @@
+package mypack;
+
+public class Methods6 {
+		  static int plusMethod(int x, int y) {
+		    return x + y;
+		  }
+		  
+		  static double plusMethod(double x, double y) {
+		    return x + y;
+		  }
+		  
+		  public static void main(String[] args) {
+		    int myNum1 = plusMethod(8, 5);
+		    double myNum2 = plusMethod(4.3, 6.26);
+		    System.out.println("int: " + myNum1);
+		    System.out.println("double: " + myNum2);
+		  }
+		}
+//we overload the plusMethod method to work for both int and double:
