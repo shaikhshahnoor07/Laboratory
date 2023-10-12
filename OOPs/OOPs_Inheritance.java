5ac698d115480c0a41eecb92aaf87993a8aca0f3
Shaikh Shahnoor Ahmed <116806924+shaikhshahnoor07@users.noreply.github.com>
2023-10-12 13:17:41 +0530

    Create OOPs_Inheritance.java

---
+++ OOPs/OOPs_Inheritance.java
@@ -0,0 +1,17 @@
+package mypack;
+
+class Shape{
+	String color;
+}
+class Triangle extends Shape{
+	
+}
+
+public class OOPs_Inheritance {
+	public static void main(String[] args) {
+		Triangle t1 = new Triangle();
+		t1.color="Red";
+	}
+
+}
+
