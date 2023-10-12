0f268d29e9c3f719083e3bfabeb3905ab49b4809
Shaikh Shahnoor Ahmed <116806924+shaikhshahnoor07@users.noreply.github.com>
2023-10-12 13:32:43 +0530

    Create Methods4.java

---
+++ Methods/Methods4.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Methods4 {
+public static void mymethod(String fname , int age) {
+	System.out.println(fname + " is " + age);
+}
+public static void main(String[] args) {
+	mymethod("shahnoor",13);
+	mymethod("hero",10);
+	mymethod("shahnwaz",18);
+}
+}
