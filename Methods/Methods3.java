64636310db007b1dfd9186aba44e9f048a19c2f0
Shaikh Shahnoor Ahmed <116806924+shaikhshahnoor07@users.noreply.github.com>
2023-10-12 13:31:51 +0530

    Create Methods3.java

---
+++ Methods/Methods3.java
@@ -0,0 +1,10 @@
+package mypack;
+public class Methods3{
+public static void mymethod(String fname){
+System.out.println(fname + " Shaikh");
+}
+public static void main(String[] args){
+mymethod("jamal");
+mymethod("Shahnoor");
+mymethod("king");
+}}
